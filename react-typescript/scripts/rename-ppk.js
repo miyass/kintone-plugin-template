d810a708efb836a987ca36a979e0ef45c1f856e5
miyass <exe.miyakura0629@gmail.com>
2021-01-06 16:37:44 +0900

    fix plugin scripts

--- react-typescript/scripts/rename-ppk.js
+++ react-typescript/scripts/rename-ppk.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
 
-fs.readdir('../dist', function(err, files){
+fs.readdir('dist', function(err, files){
   if (err) throw err;
   files.forEach(file => {
     if (file.indexOf(".ppk") !== -1) {
-      fs.rename(`../dist/${file}`, '../dist/private.ppk', err => {
+      fs.rename(`dist/${file}`, 'dist/private.ppk', err => {
         if (err) throw err;
         console.log('ppkファイルをprivate.ppkに変更しました！');
       });
     }
-  })
+  });
+  console.log('プラグイン開発の準備が整いました！');
 });
